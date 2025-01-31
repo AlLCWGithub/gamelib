9b818a42343bf47ea80ed2c6dd99f91a448e454d
Alson <alson20110204@gmail.com>
2025-01-31 12:00:55 +0800

    Added Age Verification .c and .exe files

---
+++ ageverification.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+int age;
+printf("Welcome! Please input your age!\n");
+scanf("%d", &age);
+if (age >= 18) {
+printf("You are 18 or above!");
+}
+else {
+printf("You are below 18!");
+}
+};
