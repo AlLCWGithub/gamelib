e5c018dcda3044e7d79115ea759794b6b467f231
Alson <alson20110204@gmail.com>
2025-01-31 22:33:01 +0800

    Add file trigger_all.sh

---
+++ trigger_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# List all workflows
+workflows=("tictactoe.yml" "calculator.yml" "nameflipper.yml" "newworkflow.yml")
+
+# Trigger each workflow
+for workflow in "${workflows[@]}"; do
+    echo "Triggering workflow: $workflow"
+    gh workflow run "$workflow"
+done
