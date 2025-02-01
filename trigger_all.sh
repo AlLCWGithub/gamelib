b43bc063e13e91b1e5edf05e2bc9d51db18aac77
Alson <alson20110204@gmail.com>
2025-02-01 23:04:58 +0800

    Change trigger_all.sh

--- trigger_all.sh
+++ trigger_all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # List all workflows
-workflows=("tictactoe.yml" "calculator.yml" "nameflipper.yml" "menu.yml")
+workflows=("tictactoe.yml" "calculator.yml" "nameflipper.yml" "menu.yml" "chatgptgame.yml")
+
+# Branch name (can be dynamically set or hardcoded)
+branch="main"
 
 # Trigger each workflow
 for workflow in "${workflows[@]}"; do
     echo "Triggering workflow: $workflow"
-    gh workflow run "$workflow"
+    gh workflow run "$workflow" --ref "$branch"
 done
